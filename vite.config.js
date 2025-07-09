c8cfc0294ab97e78ea53a6ad84ef9b97a263fdad
Rafael Spiljak <rafospiljak@gmail.com>
2025-07-09 14:38:26 +0200

    feat: define process env

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.NODE_ENV': '"production"',
+  },
   build: {
     outDir: 'dist', // output directory
     rollupOptions: {
